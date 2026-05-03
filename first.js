325eb305791e35e0fb24bb9d9c61a9bb6a1e5977
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-05-03 12:32:21 +0600

    Added the solution of first challenge within 6 minutes

--- first.js
+++ first.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const myTasks = [
+  { title: "Fix Mic Issue", priority: "High", completed: false },
+  { title: "Update GitHub Readme", priority: "High", completed: true },
+  { title: "Learn TypeScript", priority: "Low", completed: false },
+  { title: "Complete JS Challenge", priority: "High", completed: false },
+  { title: "Buy New Earphones", priority: "Medium", completed: true },
+];
+
+const newArr1 = [];
+
+for (let myTask of myTasks) {
+  if (myTask.priority === "High" && !myTask.completed) {
+    newArr1.push(myTask);
+  } else if (myTask.completed) {
+    myTask.title = `${myTask.title}!`
+  }
+}
+
+console.log(newArr1);
+console.log(myTasks);
