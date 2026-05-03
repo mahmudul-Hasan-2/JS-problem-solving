7432fefd022d0feb699dbed286dcafa53d3d88da
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-05-03 14:22:29 +0600

    Added the solution of destructuring related basic  challenges within 5 minutes

---
+++ destructuring.js
@@ -0,0 +1,34 @@
+// const student = {
+//   id: 101,
+//   fullName: "Mahmudul Hasan",
+//   gpa: 4.95,
+//   isPassed: true,
+// };
+
+// // তোমার কাজ: fullName এবং gpa বের করে প্রিন্ট করা।
+
+// const { fullName, gpa } = student;
+// console.log(fullName, gpa);
+
+// const userProfile = {
+//   username: "nirab007",
+//   address: {
+//     city: "Dhaka",
+//     street: "Mirpur 10",
+//     zipCode: 1216,
+//   },
+// };
+
+// const {
+//   address: { city, street },
+// } = userProfile;
+
+// console.log(city, street)
+
+// const colors = ["Red", "Green", "Blue", "Yellow"];
+
+// // তোমার কাজ: প্রথম রং 'firstColor' এবং দ্বিতীয় রং 'secondColor' ভেরিয়েবলে স্টোর করা।
+
+// const [firstColor, secondColor] = colors;
+
+// console.log(firstColor);
